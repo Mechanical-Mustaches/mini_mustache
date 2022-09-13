2bfec3242a86476dce2f14e3ed0a73a96a303615
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2022-09-12 19:30:41 -0500

    prepare for javascript yukies!!!

--- code/agents_arrive/agents/web_page.py
+++ code/agents_arrive/agents/web_page.py
@@ -2,9 +2,11 @@ import mechanical_mustaches as mm
 from mechanical_mustaches import m
 import mechanical_mustaches.web.picoweb as picoweb
 import mechanical_mustaches.web.ulogging as logging
+import json
+import esp32
 logging.basicConfig(level=logging.INFO)
 
-site = picoweb.WebApp(__name__)
+
 
 form_data = None
 
@@ -44,24 +46,66 @@ def index(req, resp):
         <style>html{background-color: #0e1ce3; font-family: Helvetica; display:inline-block; margin: 0px auto; text-align: center;}
   h1{color: #ffffff; padding: 2vh;}p{font-size: 1.5rem;}.button{display: inline-block; background-color: #e7bd3b; border: none;
   border-radius: 4px; color: white; padding: 16px 40px; text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}
-  .button2{background-color: #ff0dba;}.button3{background-color: #06876f;}.button4{background-color: #eb3440;}</style>
+  .button2{background-color: #ff0dba;}.button3{background-color: #06876f;}.button4{background-color: #eb3440;}.myDiv{display: block;margin-left: auto;
+  margin-right: auto; width: 300px;
+  border: 5px outset #2E8B57; background-color: lightblue; text-align: center;}</style>
+  <script>
+var source = new EventSource("cnc/events");
+source.onmessage = function(event) {
+    var load = JSON.parse(event.data);
+    console.log(load);
+    document.getElementById("temp").innerHTML = load.temp;
+    document.getElementById("count").innerHTML = load.count;
+}
+source.onerror = function(error) {
+    console.log(error);
+    document.getElementById("result").innerHTML += "EventSource error:" + error + "<br>";
+}
+</script>
     </head>
     <body>
     <h1>Mo's Mayhem</h1>
+    <h3>machine status: </h3><br>
+<strong>
+Count: <div class="myDiv" id="count"></div>
+Status: <div class="myDiv" id="status"></div>
+Position: <div class="myDiv" id="position"></div>
+Temp: <div class="myDiv" id="temp"></div></strong>
+<br>
+<br>
     <p>""")
     for func in funcs:
         yield from resp.awrite(button(func))
     yield from resp.awrite('</p></html>')
     
-@site.route("/")
-def index(req, resp):
-    yield from picoweb.start_response(resp)
-    yield from resp.awrite(web_page())
+    
+    
 
-@site.route("/exit")
-def exit(req, resp):
-    print('exiting')
-    raise KeyboardInterrupt
-print('loading run')
+def events(req, resp):
+    print("Event source connected")
+    yield from resp.awrite("HTTP/1.0 200 OK\r\n")
+    yield from resp.awrite("Content-Type: text/event-stream\r\n")
+    yield from resp.awrite("\r\n")
+    i = 0
+    try:
+        while True:
+            load = {"count": i,
+                    "temp": esp32.raw_temperature()}
+            load = "data: {}\n\n".format(json.dumps(load))
+            yield from resp.awrite(load)
+            yield from uasyncio.sleep(.5)
+            i += 1
+    except OSError:
+        print("Event source connection closed")
+        yield from resp.aclose()
+
+
+
+site = picoweb.WebApp(__name__, ROUTES)
+
+ROUTES = [
+    ("/", index),
+    ("/events", events),
+]
 site.run(debug=1, port=80, host=mm.my_ip)
 
