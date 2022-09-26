f9f8b77c34922f930607d7c3989e81eb03d562bc
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2022-09-26 06:26:22 -0500

    missed main

--- code/agents_arrive/main.py
+++ code/agents_arrive/main.py
@@ -2,11 +2,10 @@ import mechanical_mustaches as mm
 from mechanical_mustaches import m
 from robot import *
 
-# mm.wifi_connect('ATTB4Wg5vS', '34pm#mi3ms7%')
-# mm.wifi_connect("I46311W69080041 9758", '9J457h6$')
+
 mm.wifi_connect()
-import agents.web_page
+
+mm.start_web_page()
 
 m.run(Robot())
 
- 
