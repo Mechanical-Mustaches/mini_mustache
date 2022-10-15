f93759dc24e069ded90560914917765d5a2223ef
Andrew Wingate <1464801+anfroholic@users.noreply.github.com>
2022-10-15 14:09:37 -0500

    larry senses trouble!! :)
    Thanks for joining us 4787 Axiom!!!

--- code/agents_arrive/main.py
+++ code/agents_arrive/main.py
@@ -1,31 +1,31 @@
-
-import config
-config.wifi_connect()
-
-
-import mechanical_mustaches as mm
-from mechanical_mustaches import m
-
-
-
-mm.start_web_page()
-import sys
-# webrepl.start()
-try:
-    from robot import *
-    config.ss.fill(0,0,0)
-    m.run(Robot())
-except Exception as e:
-    sys.print_exception(e)
-    with open('/mechanical_mustaches/web/errors.log', 'w') as f:
-        sys.print_exception(e, f)
-    config.ss.fill(5,0,0)
-    m.post("BOOT COMPLETE")
-    m.post("WITH ERRORS")
-    import uasyncio
-    loop = uasyncio.get_event_loop()
-    loop.run_forever()
-    
+# import wifi_cfg
+# import config
+# config.wifi_connect()
+# 
+# 
+# import mechanical_mustaches as mm
+# from mechanical_mustaches import m
+# 
+# 
+# 
+# mm.start_web_page()
+# import sys
+# # webrepl.start()
+# try:
+#     from robot import *
+#     config.ss.fill(0,0,0)
+#     m.run(Robot())
+# except Exception as e:
+#     sys.print_exception(e)
+#     with open('/mechanical_mustaches/web/errors.log', 'w') as f:
+#         sys.print_exception(e, f)
+#     config.ss.fill(5,0,0)
+#     m.post("BOOT COMPLETE")
+#     m.post("WITH ERRORS")
+#     import uasyncio
+#     loop = uasyncio.get_event_loop()
+#     loop.run_forever()
+#     
 
 
   #----------------------------------
